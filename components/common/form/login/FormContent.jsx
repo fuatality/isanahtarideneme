51481258103202d501964a69fac4c0f67339156f
fuatality <73358641+fuatality@users.noreply.github.com>
2023-07-30 10:11:26 +0000

    searchform validation

--- components/common/form/login/FormContent.jsx
+++ components/common/form/login/FormContent.jsx
@@ -10,11 +10,35 @@ import { setRole } from "../../../../features/user/userSlice"; // replace with a
 const FormContent = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState(""); // Add this line
+  const [passwordError, setPasswordError] = useState(""); // Add this line
   const router = useRouter();
-  const dispatch = useDispatch(); // Add this line
+  const dispatch = useDispatch();
+
+  // Function to validate email format
+  const validateEmail = (email) => {
+    const pattern = /^[^@]+@[^@]+\.[^@]+$/;
+    return pattern.test(email);
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    // Clear previous errors
+    setEmailError("");
+    setPasswordError("");
+
+    // Validate email
+    if (!validateEmail(email)) {
+      setEmailError("Invalid email format");
+      return;
+    }
+
+    // Validate password (non-empty for login)
+    if (password.trim().length === 0) {
+      setPasswordError("Password cannot be empty");
+      return;
+    }
 
     try {
       const response = await axios.post("/api/login", { email, password });
@@ -26,16 +50,16 @@ const FormContent = () => {
       Cookies.set('userRole', userRole);  // Save the user's role in a cookie
 
       // Update the user role in the Redux store
-      dispatch(setRole(userRole)); // Add this line
+      dispatch(setRole(userRole));
 
       // Handle successful login
       console.log(data);
       
       // Use router.push to redirect
       if (userRole === 'admin') {
-        router.push('/employers-dashboard/dashboard')
+        router.push('/employers-dashboard/dashboard');
       } else if (userRole === 'user') {
-        router.push('/candidates-dashboard/dashboard')
+        router.push('/candidates-dashboard/dashboard');
       }
     } catch (error) {
       // Handle error
@@ -57,6 +81,7 @@ const FormContent = () => {
             placeholder="@"
             required
           />
+          {emailError && <div className="error">{emailError}</div>} {/* Display email error */}
         </div>
 
         <div className="form-group">
@@ -68,6 +93,7 @@ const FormContent = () => {
             placeholder="****"
             required
           />
+          {passwordError && <div className="error">{passwordError}</div>} {/* Display password error */}
         </div>
 
         <div className="form-group">
@@ -86,7 +112,6 @@ const FormContent = () => {
           <Link href="/register">
             Kayıt Ol
           </Link>
-
         </div>
 
         <div className="divider">
--- components/common/form/register/FormContent.jsx
+++ components/common/form/register/FormContent.jsx
@@ -5,7 +5,6 @@ import { useDispatch } from 'react-redux';
 import { setRole } from "../../../../features/user/userSlice";
 import { useRouter } from 'next/router';
 
-
 const FormContent = () => {
   const [fullname, setFullname] = useState("");
   const [username, setUsername] = useState("");
@@ -20,9 +19,64 @@ const FormContent = () => {
   const router = useRouter();
   const dispatch = useDispatch();
 
+  // Error states
+  const [fullnameError, setFullnameError] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [phoneError, setPhoneError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [passwordConfirmationError, setPasswordConfirmationError] = useState("");
+  const [fileError, setFileError] = useState("");
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-  
+
+    // Clear previous errors
+    setFullnameError("");
+    setEmailError("");
+    setPhoneError("");
+    setPasswordError("");
+    setPasswordConfirmationError("");
+    setFileError("");
+
+    // Full name validation
+    if (fullname.trim().length === 0) {
+      setFullnameError("Full name cannot be empty");
+      return;
+    }
+
+    // Email validation
+    const emailPattern = /^[^@]+@[^@]+\.[^@]+$/;
+    if (!emailPattern.test(username)) {
+      setEmailError("Invalid email format");
+      return;
+    }
+
+    // Phone number validation (optional)
+    const phonePattern = /^[0-9]{10}$/; // Example pattern for 10-digit phone number
+    if (!phonePattern.test(phone)) {
+      setPhoneError("Invalid phone number");
+      return;
+    }
+
+    // Password validation
+    if (password.trim().length < 8) {
+      setPasswordError("Password must be at least 8 characters long");
+      return;
+    }
+
+    // Password confirmation validation
+    if (password !== passwordConfirmation) {
+      setPasswordConfirmationError("Passwords do not match");
+      return;
+    }
+
+    // File validation (optional)
+    if (documents && !['.pdf', '.jpeg', '.doc', '.docx'].includes(documents.name.split('.').pop())) {
+      setFileError("Invalid file type");
+      return;
+    }
+
+    // Form data preparation
     const formData = new FormData();
     formData.append('fullname', fullname);
     formData.append('username', username);
@@ -34,16 +88,17 @@ const FormContent = () => {
     formData.append('dataProcessingConsent', dataProcessingConsent);
     formData.append('communicationConsent', communicationConsent);
     formData.append('termsConsent', termsConsent);
-  
+
     try {
       const response = await axios.post("/api/register", formData);
       const data = response.data;
       const token = data.token;
+      const userRole = data.userRole; // Extract the user role from the response data
       Cookies.set('jwt', token);
-      Cookies.set('userRole', userRole);  // Save the user's role in a cookie
+      Cookies.set('userRole', userRole); // Save the user's role in a cookie
 
       dispatch(setRole(userRole));
-  
+
       // Handle successful registration
       console.log(data);
       
@@ -52,34 +107,33 @@ const FormContent = () => {
       // Handle error
       console.log(error);
     }
-};
-
-  
+  };
 
   const handleFileChange = (e) => {
     setDocuments(e.target.files[0]);
   };
 
-  // Render the form
   return (
     <form onSubmit={handleSubmit} encType="multipart/form-data">
-
       {/* name */}
       <div className="form-group">
         <label>Ad Soyad</label>
         <input type="text" name="fullname" placeholder="Ad Soyad" required onChange={(e) => setFullname(e.target.value)} />
+        {fullnameError && <div className="error">{fullnameError}</div>}
       </div>
 
       {/* email */}
       <div className="form-group">
         <label>E-mail Adresi</label>
         <input type="email" name="username" placeholder="E-mail Adresi" required onChange={(e) => setUsername(e.target.value)} />
+        {emailError && <div className="error">{emailError}</div>}
       </div>
 
       {/* phone number */}
       <div className="form-group">
         <label>Telefon Numarası</label>
         <input type="tel" name="phone" placeholder="Telefon Numarası" required onChange={(e) => setPhone(e.target.value)} />
+        {phoneError && <div className="error">{phoneError}</div>}
       </div>
 
       {/* date */}
@@ -91,19 +145,22 @@ const FormContent = () => {
       {/* password */}
       <div className="form-group">
         <label>Şifre Oluştur</label>
-        <input id="password-field" type="password" name="password" placeholder="Şifre Oluştur" required onChange={(e) => setPassword(e.target.value)} />
+        <input type="password" name="password" placeholder="Şifre Oluştur" required onChange={(e) => setPassword(e.target.value)} />
+        {passwordError && <div className="error">{passwordError}</div>}
       </div>
 
       {/* password confirmation */}
       <div className="form-group">
         <label>Şifrenizi tekrar giriniz</label>
         <input type="password" name="passwordConfirmation" placeholder="Şifrenizi tekrar giriniz" required onChange={(e) => setPasswordConfirmation(e.target.value)} />
+        {passwordConfirmationError && <div className="error">{passwordConfirmationError}</div>}
       </div>
 
       {/* file upload */}
       <div className="form-group">
         <label>CV'ni Yükle</label>
         <input type="file" name="documents" accept=".pdf,.jpeg,.doc,.docx"  onChange={handleFileChange} />
+        {fileError && <div className="error">{fileError}</div>}
       </div>
 
       {/* checkboxes */}
@@ -128,7 +185,6 @@ const FormContent = () => {
           Kaydol
         </button>
       </div>
-
     </form>
   );
 };
